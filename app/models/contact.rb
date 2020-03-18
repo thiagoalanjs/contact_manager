d9867f6b5ade5b7c8443423f9a20d1e16faf01c0
Thiago Alan <thiagoalan9080@gmail.com>
2020-03-17 23:29:07 -0300

    Add gravatar image profile

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :group
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}"
+  end
+  
 end
