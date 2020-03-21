e677ffce4f9a892de1fadfede479ffb7464f5ea2
Thiago Alan <thiagoalan9080@gmail.com>
2020-03-20 23:47:52 -0300

    Added Function to save user in database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  
   def index
     if params[:group_id] && !params[:group_id].empty?
         #@contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
@@ -7,4 +8,24 @@ class ContactsController < ApplicationController
        @contacts = Contact.page(params[:page])
     end
   end
+
+  def new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Contact was successfully created"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params  
+    params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'contacts/index', as: 'contacts'
+  get 'contacts/new', as: 'new_contact'
+  post '/contacts/create', to: 'contacts#create'
   root 'contacts#index'
 
 
