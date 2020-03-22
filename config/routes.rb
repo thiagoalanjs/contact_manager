80d36f2b4e1a57e5d742f644d86b7bde3d70a8fa
Thiago Alan <thiagoalan9080@gmail.com>
2020-03-21 22:37:31 -0300

    Fix in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   get 'contacts/index', as: 'contacts'
   get 'contacts/new', as: 'new_contact'
-  post 'contacts/create', to: 'create_contact'
+  post 'contacts/create', as: 'create_contact'
   get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
   patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
   root 'contacts#index'
+
 end
