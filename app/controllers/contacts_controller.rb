b04c3b92f1daf1de77e69368cd147f7789f458b6
Thiago Alan <thiagoalan9080@gmail.com>
2020-03-22 16:27:02 -0300

    Refactory routes and controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
-  
+  before_action :find_contact,  only: [:edit, :update, :destroy]
+
   def index
     if params[:group_id] && !params[:group_id].empty?
        #@contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
@@ -24,11 +25,10 @@ class ContactsController < ApplicationController
   end
 
   def edit
-    @contact = Contact.find(params[:id])
+    
   end
 
   def update
-    @contact = Contact.find(params[:id])
     if @contact.update(contact_params)
       flash[:success] = "Contact was successfully updated."
       redirect_to contacts_path
@@ -38,7 +38,7 @@ class ContactsController < ApplicationController
   end
   
   def destroy 
-    Contact.find(params[:id]).destroy
+    @contact.destroy 
       flash[:success] = "Contact was successfully deleted."
       redirect_to contacts_path
   end
@@ -48,6 +48,10 @@ class ContactsController < ApplicationController
 
   def contact_params  
     params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id, :avatar)
+  end
+
+  def find_contact
+    @contact = Contact.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'contacts/index', as: 'contacts'
-  get 'contacts/new', as: 'new_contact'
-  post 'contacts/create', as: 'create_contact'
-  get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
-  patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
-  delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
+  # get 'contacts/index', as: 'contacts'
+  # get 'contacts/new', as: 'new_contact'
+  # post 'contacts/create', as: 'create_contact'
+  # get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
+  # patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
+  # delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
+  resources :contacts, except: [:show]
   root 'contacts#index'
-
+  
 end
